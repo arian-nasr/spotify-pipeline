4e002e952542f31f8da8a7621d23371e63e6a936
Arian Nasr <81041177+arian-nasr@users.noreply.github.com>
2025-11-15 14:46:38 -0500

    add track delete functionality frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,8 @@
-const apiUrl = 'http://spotify-grabber-docker:5000/tracks';
+const apiUrl = 'http://spotify-grabber-docker:5000';
 
 async function fetchTracks() {
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(`${apiUrl}/tracks`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -18,12 +18,37 @@ function displayTracks(tracks) {
     const trackList = document.getElementById('track-list');
     trackList.innerHTML = '';
 
-    tracks.tracks.forEach(track => {
+    tracks.tracks.forEach((track, index) => {
         const listItem = document.createElement('li');
         listItem.textContent = `${track.track_name} by ${track.artist_names}`;
+        
+        // Add click event to delete track
+        listItem.addEventListener('click', function() {
+            fetch(`${apiUrl}/tracks/delete/${encodeURIComponent(track.track_name)}/${encodeURIComponent(track.artist_names)}`)
+                .then(response => {
+                    if (!response.ok) { 
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data.message);
+                })
+                .catch(error => {
+                    console.error('Error deleting track:', error);
+                });
+
+            fetchTracks().then(tracks => {
+                displayTracks(tracks);
+            });
+
+            displayTracks(tracks);
+        });
+        
         trackList.appendChild(listItem);
     });
 }
+
 
 document.addEventListener('DOMContentLoaded', async () => {
     const tracks = await fetchTracks();
